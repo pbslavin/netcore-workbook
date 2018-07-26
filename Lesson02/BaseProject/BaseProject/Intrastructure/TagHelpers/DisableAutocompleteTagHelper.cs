7c3d32c4295f5fab67094bb110e721bc1bdb099e
Peter Slavin <pbslavin@gmail.com>
2018-07-26 11:34:12 -0500

    changed target of disable-autocomplete to input

--- Lesson02/BaseProject/BaseProject/Intrastructure/TagHelpers/DisableAutocompleteTagHelper.cs
+++ Lesson02/BaseProject/BaseProject/Intrastructure/TagHelpers/DisableAutocompleteTagHelper.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BaseProject.Intrastructure.TagHelpers
 {
-    [HtmlTargetElement("form", TagStructure = TagStructure.NormalOrSelfClosing)]
+    [HtmlTargetElement("input")]
     public class DisableAutocompleteTagHelper : TagHelper
     {
         public override void Process(TagHelperContext context, TagHelperOutput output)
